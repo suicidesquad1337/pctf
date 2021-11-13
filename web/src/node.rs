7d2d6ecd3f657a8a5768e6ce10a34dcaf83265f4
Erik Tesar <erik@erik-tesar.com>
2021-11-13 20:40:07 +0100

    web: implement `node` query and fix typo

--- web/src/node.rs
+++ web/src/node.rs
@@ -1,9 +1,12 @@
 //! Defines the node interface. This trait should be implemented by all types
 //! with an unique identifier
 
-use async_graphql::{Interface, ID};
+use std::str::FromStr;
 
-use crate::challenge::Challenge;
+use async_graphql::{dataloader::DataLoader, Context, Interface, Object, Result, ID};
+use uuid::Uuid;
+
+use crate::{challenge::Challenge, loaders::ChallengeLoaderByID};
 
 // interfaces in graphql are like traits in rust with downcasting, however,
 // async_graphql does not support trait objects for interface. Instead, it uses
@@ -15,5 +18,26 @@ use crate::challenge::Challenge;
 ///
 /// [1]: https://graphql.org/learn/global-object-identification/
 pub enum Node {
-    Chalenge(Challenge),
+    Challenge(Challenge),
+}
+
+#[non_exhaustive]
+#[derive(Default, Debug)]
+/// The `node` query object
+pub struct NodeQuery;
+
+#[Object]
+impl NodeQuery {
+    // get a node with `id`. If the `Node` does not exist, it returns `null`.
+    async fn node(&self, ctx: &Context<'_>, id: ID) -> Result<Option<Node>> {
+        // currently, only `Challenge` is a `Node`, so we can directly query for
+        // that
+        let challenge: Option<_> = ctx
+            .data_unchecked::<DataLoader<ChallengeLoaderByID>>()
+            .load_one(Uuid::from_str(&id.0)?)
+            .await?;
+
+        // cant use into on Option even tho Node implements From<Challenge>
+        Ok(challenge.map(|c| c.into()))
+    }
 }
